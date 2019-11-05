15ba61f9b7af1ae3d3ad615cdd3ff4f7250dc9d9
Mahendra Prajapati <mahendra.prajapati@comviva.com>
2019-11-05 16:04:31 +0530

    Second

--- src/main/java/com/soma/calculator/Calculator.java
+++ src/main/java/com/soma/calculator/Calculator.java
@@ -2,23 +2,23 @@ package com.soma.calculator;
 
 public class Calculator {
 
-	public static double add(double a, double b) {
+	public static int add(int a, int b) {
 		return a + b;
 	}
 
-	public static double subtract(double a, double b) {
+	public static int subtract(int a, int b) {
 		return a - b;
 	}
 
-	public static double multiply(double a, double b) {
+	public static int multiply(int a, int b) {
 		return a * b;
 	}
 
-	public static double divide(double a, double b) {
+	public static int divide(int a, int b) {
 		return a / b;
 	}
 
-	public static double mod(double a, double b) {
+	public static int mod(int a, int b) {
 		return a % b;
 	}
 
